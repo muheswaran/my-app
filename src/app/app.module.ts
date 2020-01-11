a007adc2b23a6a8a88bec115cd48cbc0848d2578
muheswaran <59173432+muheswaran@users.noreply.github.com>
2020-01-11 16:44:35 +0530

    component
    created a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Home2Component } from './home2/home2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Home2Component
   ],
   imports: [
     BrowserModule,
