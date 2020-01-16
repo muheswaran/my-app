424189cb0a570f95d6c6b44162c6ccfb8daa2cef
muheswaran <59173432+muheswaran@users.noreply.github.com>
2020-01-16 22:41:37 +0530

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Home2Component } from './home2/home2.component';
-
+import { TopNavComponent } from './header-module/top-nav/top-nav.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Home2Component
+    Home2Component,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
